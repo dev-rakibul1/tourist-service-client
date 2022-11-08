49a7f08f1de4c5f48874a226ed6e9164a82fe64c
dev-rakibul1 <fl.rakibul@gmail.com>
2022-11-08 23:15:26 +0600

    form validation is done

--- src/components/pages/booking/Booking.jsx
+++ src/components/pages/booking/Booking.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 
 const Booking = () => {
-  const [bookData, setBookData] = useState([]);
+  const [userMess, setUserMess] = useState("");
+
   //   const data = useLoaderData();
   //   console.log(data);
 
@@ -20,10 +21,43 @@ const Booking = () => {
     event.preventDefault();
     const form = event.target;
     const name = form.name.value;
+    if (name === "" && name.length < 8) {
+      setUserMess("Name must be need 8 charater");
+      return;
+    } else {
+      setUserMess("");
+    }
     const email = form.email.value;
+    const re =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (re.test(email)) {
+      setUserMess("");
+    } else {
+      setUserMess("Please provide a valid email.");
+      return;
+    }
     const address = form.address.value;
+    if (address === "" && address.length < 15) {
+      setUserMess("Please must be need 15 charater");
+      return;
+    } else {
+      setUserMess("");
+    }
     const number = form.number.value;
+    if (number === "" && number.length < 11) {
+      setUserMess("Please please provide a valid number");
+      return;
+    } else {
+      setUserMess("");
+    }
     const message = form.message.value;
+    if (message === "" && message.length > 120 && message.length < 15) {
+      setUserMess("Please write (15-12) charter");
+      return;
+    } else {
+      setUserMess("");
+    }
 
     const bookingUser = {
       name: name,
@@ -99,6 +133,8 @@ const Booking = () => {
           placeholder="Type your message..."
           name="message"
         ></textarea>
+        <p className="text-red-600 text-center">{userMess}</p>
+
         <button className="btn btn-active btn-primary w-full mt-4 rounded-full">
           Confirm book
         </button>
