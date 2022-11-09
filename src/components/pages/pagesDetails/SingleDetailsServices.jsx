f082b9e73905b0e4237d486617a5f800992cffd2
dev-rakibul1 <fl.rakibul@gmail.com>
2022-11-09 23:16:24 +0600

    rating wrok is done

--- src/components/pages/pagesDetails/SingleDetailsServices.jsx
+++ src/components/pages/pagesDetails/SingleDetailsServices.jsx
@@ -1,22 +1,85 @@
-import React from "react";
-import { FaStar } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { FaStar, FaTimes, FaUser } from "react-icons/fa";
 import { Link, useLoaderData } from "react-router-dom";
 
 const SingleDetailsServices = () => {
   const { images, title, description, area, rating, charge } = useLoaderData();
+  const [userRating, setUserRating] = useState([]);
 
   const handleUserReview = (event) => {
     event.preventDefault();
 
     const form = event.target;
-    const rating1 = form.rating1.value;
+    const rating = form.rating.value;
     const message = form.message.value;
-    console.log(rating1);
+
+    if (message === " " || message.length < 15) {
+      toast.error("Min 15 charter reviews");
+      return;
+    }
+
+    const userReview = {
+      rating: rating,
+      message: message,
+      images: images,
+      title: title,
+    };
+
+    fetch("http://localhost:5000/userRating", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(userReview),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          toast.success(data.message);
+        } else {
+          toast.error(data.error);
+        }
+      })
+      .catch((err) => console.log(err));
   };
+
+  // delete request
+  const handleDeleteRating = (id) => {
+    const agree = window.confirm("Are your sure delete this reviews");
+    // console.log(id);
+
+    if (agree) {
+      fetch(`http://localhost:5000/userRating/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.success) {
+            toast.success(data.message);
+            const remaining = userRating.filter(
+              (uRating) => uRating._id !== id
+            );
+            setUserRating(remaining);
+          } else {
+            toast.error(data.error);
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  // user rating
+  useEffect(() => {
+    fetch("http://localhost:5000/userRating")
+      .then((res) => res.json())
+      .then((data) => setUserRating(data))
+      .then((err) => console.log(err));
+  }, []);
 
   return (
     <div className="md:flex gap-11 mt-16">
-      <div className="w-[65%] px-3">
+      <div className="w-[60%] px-3">
         <div className="card bg-base-100 shadow-xl">
           <figure>
             <img src={images} alt="Shoes" />
@@ -40,28 +103,41 @@ const SingleDetailsServices = () => {
               <h4 className="text-2xl">New review request</h4>
               <form className="" onSubmit={handleUserReview}>
                 {/* review area */}
-                <div className="user-rating flex ">
-                  <label htmlFor="rate-5" className="hover:text-green-800">
-                    <FaStar />
-                  </label>
-                  <input type="radio" name="rate" id="rate-5" />
-                  <label htmlFor="rate-4" className="hover:text-green-800">
-                    <FaStar />
-                  </label>
-                  <input type="radio" name="rate" id="rate-4" />
-                  <label htmlFor="rate-3" className="hover:text-green-800">
-                    <FaStar />
-                  </label>
-                  <input type="radio" name="rate" id="rate-3" />
-                  <label htmlFor="rate-2" className="hover:text-green-800">
-                    <FaStar />
-                  </label>
-                  <input type="radio" name="rate" id="rate-2" />
-                  <label htmlFor="rate-1" className="hover:text-green-800">
-                    <FaStar />
-                  </label>
-                  <input type="radio" name="rate" id="rate-1" />
+
+                <div className="rating py-4">
+                  <input
+                    type="radio"
+                    name="rating"
+                    className="mask mask-star-2 bg-orange-400"
+                    value="1"
+                  />
+                  <input
+                    type="radio"
+                    name="rating"
+                    className="mask mask-star-2 bg-orange-400"
+                    value="2"
+                  />
+                  <input
+                    type="radio"
+                    name="rating"
+                    className="mask mask-star-2 bg-orange-400"
+                    value="3"
+                  />
+                  <input
+                    type="radio"
+                    name="rating"
+                    className="mask mask-star-2 bg-orange-400"
+                    checked
+                    value="4"
+                  />
+                  <input
+                    type="radio"
+                    name="rating"
+                    className="mask mask-star-2 bg-orange-400"
+                    value="5"
+                  />
                 </div>
+
                 <textarea
                   className="textarea textarea-primary w-full h-48 text-xl	"
                   placeholder="Your review text here..."
@@ -77,7 +153,7 @@ const SingleDetailsServices = () => {
           </div>
         </div>
       </div>
-      <div className="w-[35%] px-3">
+      <div className="w-[40%] px-3">
         <article>
           <h2 className="text-2xl booking-title mt-4">
             Booking your favourite place
@@ -99,6 +175,43 @@ const SingleDetailsServices = () => {
             </button>
           </Link>
         </article>
+
+        {/* user rating */}
+        <div className="mt-7">
+          <table>
+            <tr>
+              <th>Place name</th>
+              <th>Place images</th>
+              <th>User</th>
+              <th>User meg</th>
+              <th>Rating</th>
+              <th>Delete</th>
+            </tr>
+            {userRating.map((rating) => (
+              <tr>
+                <td>{rating?.title}</td>
+                <td>
+                  <img
+                    src={rating?.images}
+                    alt="rating images"
+                    className="w-9"
+                  />
+                </td>
+                <td>
+                  <FaUser />
+                </td>
+                <td>{rating.message.slice(0, 30)}</td>
+                <td>{rating.rating} Star</td>
+                <td
+                  className="cursor-pointer"
+                  onClick={() => handleDeleteRating(rating._id)}
+                >
+                  <FaTimes />
+                </td>
+              </tr>
+            ))}
+          </table>
+        </div>
       </div>
     </div>
   );
